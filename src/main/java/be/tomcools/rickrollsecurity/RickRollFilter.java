efcdcc8c58aa2820f633e0574b3f6a1da13c217e
Martin Visser <martinvisser@gmail.com>
2021-03-11 20:51:10 +0100

    Make use of an AntPathMatcher to make checking on paths more flexible

--- src/main/java/be/tomcools/rickrollsecurity/RickRollFilter.java
+++ src/main/java/be/tomcools/rickrollsecurity/RickRollFilter.java
@@ -1,5 +1,8 @@
 package be.tomcools.rickrollsecurity;
 
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -10,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class RickRollFilter implements Filter {
+    private static final PathMatcher PATH_MATCHER = new AntPathMatcher();
 
     private final RickRollConfigurationProperties properties;
 
@@ -23,7 +27,7 @@ public class RickRollFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         String requestUri = request.getRequestURI();
         for (String path : properties.getPaths()) {
-            if (requestUri.equals(path)) {
+            if (PATH_MATCHER.match(path, requestUri)) {
                 rickroll(response);
                 return;
             }
